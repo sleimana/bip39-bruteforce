96c5c2a4fa394fed3b24af514c4c03db2da1b17e
X4ck3R <sleiman.xsp@gmail.com>
2018-05-15 11:26:49 +0200

    added dictionary attack

--- brute.py
+++ brute.py
@@ -8,7 +8,8 @@ from itertools import permutations
 seed_words = ''
 address = ''
 start_index = 1250
-sequential = True
+dictionary = ''
+attack_type = 2 #[0=>'pin_sequential', 1=>'pin_permutation', 2=>'dictionary']
 
 def _create_standard_wallet(ks):
     gap_limit = 1  # make tests run faster
@@ -19,34 +20,43 @@ def _create_standard_wallet(ks):
     w.synchronize()
     return w
 
+
 def check_bip39_seed_bip49_p2sh_segwit(password, index):
-    
     ks = from_bip39_seed(seed_words, password, "m/49'/0'/0'")
     w = _create_standard_wallet(ks)
     addr = w.get_receiving_addresses()[0]
-	
+
     if (addr == address):
         print(index + '\t PASSWORD FOUND: ' + password)
         return True
     else:
-        print(index +'\t'+ str(addr)+ '\t' + password)
+        print(index + '\t' + str(addr) + '\t' + password)
         return False
 
+def readDic (dic):
+    with open(dic) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    return content
 
-if (sequential):
-	for i in range(0,9999):
-	   pwd = str(i).zfill(4)
-	   print()
-	   #check_bip39_seed_bip49_p2sh_segwit(str(pwd),str(i))
-	
-else:
+if (attack_type == 0):
+    for i in range(0, 9999):
+        pwd = str(i).zfill(4)
+        print()
+        # check_bip39_seed_bip49_p2sh_segwit(str(pwd),str(i))
+
+elif(attack_type == 1):
     pin = '1 2 3 4 5 6 7 8 9'
     words = pin.split()
-    i=0
+    i = 0
     for p in permutations(words, 9):
-		    i=i+1
-		    if (i<start_index):
-			    continue
-		    pwd = "".join(str(x) for x in p)
-		    check_bip39_seed_bip49_p2sh_segwit(str(pwd),str(i))
+        i = i + 1
+        if (i < start_index):
+            continue
+        pwd = "".join(str(x) for x in p)
+        check_bip39_seed_bip49_p2sh_segwit(str(pwd), str(i))
 
+elif(attack_type == 2):
+    w = readDic('bip39list.txt')
+    for i in range(0, 2048):
+        check_bip39_seed_bip49_p2sh_segwit(str(w[i]), str(i))
